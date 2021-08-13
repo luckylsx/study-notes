17aa693f4ccf0c6440f943feab2652f3485f508c
shuangxi.li <lucky9303@163.com>
2021-08-13 23:35:58 +0800

    code

---
+++ CodeStandards/test.go
@@ -0,0 +1,14 @@
+package codestandards
+public List<int[]> getThem () {
+    List<int[]> listl = new ArrayList<int[]>() ;
+    for (int [] x : theList)
+    if (x[0] == 4)
+        listl.add (x);
+    return list1;
+}
+
+var theList = make([]int)
+
+func getThem() {
+	list = 
+}
